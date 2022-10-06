3870dd0bf1be8a1ca91b9fed9dee01c0c136e403
DanielleM <dlmedeiro@gmail.com>
2022-10-06 16:28:08 -0700

    home page structure

---
+++ src/DogList.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "./DogList.css";
+import { Link } from "react-router-dom";
+// import "./images/whiskey.jpg";
+// import "./images/duke.jpg";
+// import "./images/perry.jpg";
+// import "./images/tubby.jpg";
+
+function DogList({ dogs }) {
+  // let dogInfo = dogs.dogs.dogs;
+  // const headShot = () => {};
+
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <div>
+        <h2>Doggies</h2>
+        {dogs.map((d) => (
+          <div className="doggies">
+            <h3
+            // style={{
+            //   backgroundImage: `url(${d.src})`,
+            //   height: "200px",
+            //   width: "200px",
+            // }}
+            >
+              <Link to={`/dogs/${d.name}`}>{d.name}</Link>
+            </h3>
+            <h3>{d.src}</h3>
+            <img src={d.src} alt={d.name} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default DogList;
---
+++ src/Routes.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+import DogList from "./DogList";
+// import Details from "./Details";
+
+function Routes({ dogs }) {
+  return (
+    <>
+      <Switch>
+        <Route exact path="/dogs">
+          <DogList dogs={dogs} />
+        </Route>
+        {/* <Route exact path="/dogs/:name">
+          <Details />
+        </Route> */}
+        <Redirect to="/dogs" />
+      </Switch>
+    </>
+  );
+}
+
+export default Routes;
